955bd9a4252e7f2f4949e33e2544312e2be02d3f
Victor Hugo Soares Castro <vhsoaresbr@gmail.com>
2025-02-17 11:05:10 -0300

    Finalizando o Projeto fazendo a criaçãode gráficos (a partir da linha 18)

--- main.py
+++ main.py
@@ -14,3 +14,12 @@ max_price = st.sidebar.slider ("Price range", price_min, price_max, price_min)
 df_books = df_top100_livros[df_top100_livros["book price"] <= max_price]
 
 df_books
+
+grafico_barras = px.bar (df_books["year of publication"].value_counts())
+grafico_histograma = px.histogram(df_books["book price"])
+
+col1, col2 = st.columns(2)
+
+col1.plotly_chart (grafico_barras)
+col2.plotly_chart (grafico_histograma)
+
