5b58cbce8f08f8da9dc1bb4412e318cec8fbec44
Victor Hugo Soares Castro <vhsoaresbr@gmail.com>
2025-02-17 16:06:01 -0300

    Adicionando painel dos Livros e suas respectivas revisões feitas

--- pages/books_reviews.py
+++ pages/books_reviews.py
@@ -1,9 +1,16 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(layout="wide")
+
 df_reviews = pd.read_csv ('dados/customer reviews.csv')
 df_top100_livros  = pd.read_csv ("dados/Top-100 Trending Books.csv")
 
 livros = df_top100_livros["book title"].unique()
 livro = st.sidebar.selectbox ("Books", livros)
 
+df_book = df_top100_livros[df_top100_livros["book title"] == livro]
+df_reviews_filtro = df_reviews[df_reviews["book name"] == livro]
+
+df_book
+df_reviews_filtro
