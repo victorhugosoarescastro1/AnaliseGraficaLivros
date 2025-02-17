59c93ff8713513fc917962b2dcb10407b0312865
Victor Hugo Soares Castro <vhsoaresbr@gmail.com>
2025-02-17 08:55:15 -0300

    +2 arquivos csv, +plotly, + linha10 (rodando o gráfico)

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import pandas as pd
+import plotly as pt
 
 st.set_page_config(layout="wide")
 
-df_reviews = pd.read_csv ("Documents\archive\ costumer reviews.csv")
-df_top100_livros  = pd.read_csv ("Documents\archive\ Top-100 Trending Books.csv")
+df_reviews = pd.read_csv ('dados/customer reviews.csv')
+df_top100_livros  = pd.read_csv ("dados/Top-100 Trending Books.csv")
+
+df_reviews
