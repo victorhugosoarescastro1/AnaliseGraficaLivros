74d98da1c3ddf68b21d9663f4aa67894137600bf
Victor Hugo Soares Castro <vhsoaresbr@gmail.com>
2025-02-17 10:44:35 -0300

    Corringindo erros lihha 9 e 10 (book price)

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import streamlit as st
 import pandas as pd
-import plotly as pt
 
 st.set_page_config(layout="wide")
 
 df_reviews = pd.read_csv ('dados/customer reviews.csv')
 df_top100_livros  = pd.read_csv ("dados/Top-100 Trending Books.csv")
 
-price_max = df_top100_livros ["Book Price"].max()
-price_min = df_top100_livros ["Book Price"].min()
+price_max = df_top100_livros["book price"].max()
+price_min = df_top100_livros["book price"].min()
 
 max_price = st.sidebar.slider ("Price range", price_min, price_max, price_min)
-df_books = df_top100_livros[df_top100_livros["Book Price"] <= max_price]
+df_books = df_top100_livros[df_top100_livros["book price"] <= max_price]
 
 df_books
