da0a2014c19a5017c8f0ef00a4c8f7eff19b87a0
Victor Hugo Soares Castro <vhsoaresbr@gmail.com>
2025-02-17 16:01:03 -0300

    Adicionando caixa de seleção na nova aba

--- pages/books_reviews.py
+++ pages/books_reviews.py
@@ -1,3 +1,9 @@
 import streamlit as st
+import pandas as pd
 
-st.write("Teste")
+df_reviews = pd.read_csv ('dados/customer reviews.csv')
+df_top100_livros  = pd.read_csv ("dados/Top-100 Trending Books.csv")
+
+livros = df_top100_livros["book title"].unique()
+livro = st.sidebar.selectbox ("Books", livros)
+
