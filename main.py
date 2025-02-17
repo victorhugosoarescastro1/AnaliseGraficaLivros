2aceac882a36ad5ed123a471974e47947e5ac926
Victor Hugo Soares Castro <vhsoaresbr@gmail.com>
2025-02-17 08:37:48 -0300

    Definição

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import streamlit as st
+import pandas as pd
 
-st.write("Olá Mundo")
+st.set_page_config(layout="wide")
+
+df_reviews = pd.read_csv ("Documents\archive\ costumer reviews.csv")
+df_top100_livros  = pd.read_csv ("Documents\archive\ Top-100 Trending Books.csv")
