4b292151e83e1cc76b213d70bd2908bfc1f4e084
Victor Hugo Soares Castro <vhsoaresbr@gmail.com>
2025-02-17 16:20:05 -0300

    Adicionando novas colunas no arquivo book_reviews

--- pages/books_reviews.py
+++ pages/books_reviews.py
@@ -17,3 +17,12 @@ genero_livro = df_book ["genre"].iloc[0]
 valor_livro = df_book ["price"].iloc[0]
 avaliacao_livro = df_book ["rating"].iloc[0]
 ano_livro = df_book ["year of publication"].iloc[0]
+
+st.title(titulo_livro)
+st.subheader(genero_livro)
+
+col1, col2, col3 = st.columns(3)
+
+col1.metric ("Preço", valor_livro)
+col2.metric ("Avaliação", avaliacao_livro)
+col3.metric |("Ano Publicado", ano_livro)
