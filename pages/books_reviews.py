e4eff36ff66bcfdcbcefc5d4bd75e987e96de69c
Victor Hugo Soares Castro <vhsoaresbr@gmail.com>
2025-02-17 16:22:20 -0300

    Mudança na linha 17 no books_reviews sobre valor_livro

--- pages/books_reviews.py
+++ pages/books_reviews.py
@@ -14,7 +14,7 @@ df_reviews_filtro = df_reviews[df_reviews["book name"] == livro]
 
 titulo_livro = df_book ["book title"].iloc[0]
 genero_livro = df_book ["genre"].iloc[0]
-valor_livro = df_book ["price"].iloc[0]
+valor_livro = f"${df_book ['price'].iloc[0]}"
 avaliacao_livro = df_book ["rating"].iloc[0]
 ano_livro = df_book ["year of publication"].iloc[0]
 
