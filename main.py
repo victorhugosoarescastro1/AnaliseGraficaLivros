7baf609040bc1eb41e66f01d6ba45ea62fa858a1
Victor Hugo Soares Castro <vhsoaresbr@gmail.com>
2025-02-17 10:19:43 -0300

    Adicionando o deslizamento de preços (slider) ao streamlit

--- main.py
+++ main.py
@@ -7,4 +7,10 @@ st.set_page_config(layout="wide")
 df_reviews = pd.read_csv ('dados/customer reviews.csv')
 df_top100_livros  = pd.read_csv ("dados/Top-100 Trending Books.csv")
 
-df_reviews
+price_max = df_top100_livros ["Book Price"].max()
+price_min = df_top100_livros ["Book Price"].min()
+
+max_price = st.sidebar.slider ("Price range", price_min, price_max, price_min)
+df_books = df_top100_livros[df_top100_livros["Book Price"] <= max_price]
+
+df_books
