db2ec08f461044783907cf1554b2d802304e2e70
Victor Hugo Soares Castro <vhsoaresbr@gmail.com>
2025-02-18 08:32:37 -0300

    Vigésimo Commit

--- pages/books_reviews.py
+++ pages/books_reviews.py
@@ -12,11 +12,11 @@ livro = st.sidebar.selectbox ("Books", livros)
 df_book = df_top100_livros[df_top100_livros["book title"] == livro]
 df_reviews_filtro = df_reviews[df_reviews["book name"] == livro]
 
-titulo_livro = df_book ["book title"].iloc[0]
-genero_livro = df_book ["genre"].iloc[0]
-valor_livro = f"${df_book ['price'].iloc[0]}"
-avaliacao_livro = df_book ["rating"].iloc[0]
-ano_livro = df_book ["year of publication"].iloc[0]
+titulo_livro = df_book["book title"].iloc[0]
+genero_livro = df_book["genre"].iloc[0]
+valor_livro = f"${df_book['book price'].iloc[0]}"
+avaliacao_livro = df_book["rating"].iloc[0]
+ano_livro = df_book["year of publication"].iloc[0]
 
 st.title(titulo_livro)
 st.subheader(genero_livro)
@@ -25,7 +25,7 @@ col1, col2, col3 = st.columns(3)
 
 col1.metric ("Preço", valor_livro)
 col2.metric ("Avaliação", avaliacao_livro)
-col3.metric |("Ano Publicado", ano_livro)
+col3.metric ("Ano Publicado", ano_livro)
 
 st.divider()
 
